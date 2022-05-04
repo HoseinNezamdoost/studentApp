d31392ef5545578155cb4afce06e8e164ce27612
hosein.nzd <hosein.nzd26@gmail.com>
2022-05-05 03:35:22 +0430

    first commit but with a delay

---
+++ app/src/main/java/com/hosein/nzd/students/AdapterStudent.java
@@ -0,0 +1,60 @@
+package com.hosein.nzd.students;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterStudent extends RecyclerView.Adapter<AdapterStudent.StudentViewHolder> {
+
+    List<Student> students = new ArrayList<>();
+
+    public AdapterStudent(List<Student> students) {
+        this.students = students;
+    }
+
+    @NonNull
+    @Override
+    public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new StudentViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_student_rv , parent , false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentViewHolder holder, int position) {
+        holder.bind(students.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return students.size();
+    }
+
+    public class StudentViewHolder extends RecyclerView.ViewHolder{
+
+        TextView fullNameTV, firstNameCharacterTV , courseTV, scoreTV;
+
+        public StudentViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            fullNameTV = itemView.findViewById(R.id.full_name_item);
+            firstNameCharacterTV = itemView.findViewById(R.id.firstname_character_item);
+            courseTV = itemView.findViewById(R.id.course_item);
+            scoreTV = itemView.findViewById(R.id.score_item);
+        }
+
+        public void bind (Student student){
+            fullNameTV.setText(student.getFirstName() + " " + student.getLastName());
+            firstNameCharacterTV.setText(student.getFirstName().substring(0 , 1));
+            courseTV.setText(student.getCourse());
+            scoreTV.setText(String.valueOf(student.getScore()));
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/hosein/nzd/students/MainActivity.java
@@ -0,0 +1,92 @@
+package com.hosein.nzd.students;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    RequestQueue requestQueue;
+    List<Student> students = new ArrayList<>();
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        ProgressBar progressBar = findViewById(R.id.progress);
+
+        StringRequest request = new StringRequest(Request.Method.GET, "https://hosein-nzd.ir/android_app/student/getStudent.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                Student student = new Student();
+                                student.setFirstName(jsonObject.getString("firstName"));
+                                student.setLastName(jsonObject.getString("lastName"));
+                                student.setCourse(jsonObject.getString("course"));
+                                student.setScore(jsonObject.getInt("score"));
+                                student.setId(jsonObject.getInt("id"));
+
+                                students.add(student);
+
+                            }
+
+                            progressBar.setVisibility(View.GONE);
+                            recyclerView = findViewById(R.id.recycler_student);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this , RecyclerView.VERTICAL , false));
+                            recyclerView.setAdapter(new AdapterStudent(students));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this,"1"+ e.getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "2"+error.getMessage(), Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+    }
+}
